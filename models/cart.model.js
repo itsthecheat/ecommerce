74eeafcc6e34e2fa123a1beab611b76a44556361
Leslie Behum <llaurenb@gmail.com>
2020-08-21 16:59:14 -0400

    add mongoose, create db schemas, test

---
+++ models/cart.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const cartSchema = new Schema({
+  user_id: { type: String, required: true, trim: true },
+  product_id: { type: String, required: true, trim: true },
+  price: { type: Number, required: true, trim: true },
+  title: { type: String, required: true, trim: true }
+}, {
+  timestamps: true,
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 import express from 'express';
 
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
