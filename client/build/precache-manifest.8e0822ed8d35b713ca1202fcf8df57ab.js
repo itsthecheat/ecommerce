51bb32c4f5fc12c796df21c0e8926d120f8f61b3
Leslie Behum <llaurenb@gmail.com>
2021-09-03 12:43:47 -0400

    in client folder remove build from gitignore and push

---
+++ client/build/precache-manifest.8e0822ed8d35b713ca1202fcf8df57ab.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2911b20c4ac41158d577e51ead5dbd8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbaa112f1b91ee890abf",
+    "url": "/static/css/2.021a5265.chunk.css"
+  },
+  {
+    "revision": "9844c05cdd20108d1040",
+    "url": "/static/css/main.2d507215.chunk.css"
+  },
+  {
+    "revision": "cbaa112f1b91ee890abf",
+    "url": "/static/js/2.adc2bf4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.adc2bf4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9844c05cdd20108d1040",
+    "url": "/static/js/main.abcd0d0c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dc5b91ab2e6aba0ec26cccf6dac6ef71",
+    "url": "/static/media/bebas-neue-latin-400.dc5b91ab.woff"
+  },
+  {
+    "revision": "e2b2c0d01e61f177b020ff2381008139",
+    "url": "/static/media/bebas-neue-latin-400.e2b2c0d0.woff2"
+  },
+  {
+    "revision": "75f7f84d6603ecff41cb91457e09b4fb",
+    "url": "/static/media/jost-all-400-italic.75f7f84d.woff"
+  },
+  {
+    "revision": "e3a7382dd7e27a2356c7246b013b0802",
+    "url": "/static/media/jost-all-400-normal.e3a7382d.woff"
+  },
+  {
+    "revision": "fc709258a5837eedd57c8c47023d0889",
+    "url": "/static/media/jost-cyrillic-400-italic.fc709258.woff2"
+  },
+  {
+    "revision": "74a2720fc7788f57ebadab56647fd15f",
+    "url": "/static/media/jost-cyrillic-400-normal.74a2720f.woff2"
+  },
+  {
+    "revision": "c04b5041f7d3e94b10dd0f18853d3283",
+    "url": "/static/media/jost-latin-400-italic.c04b5041.woff2"
+  },
+  {
+    "revision": "1197de9fb65047e2b477cb3a5905e4ce",
+    "url": "/static/media/jost-latin-400-normal.1197de9f.woff2"
+  },
+  {
+    "revision": "2fd6c606d1bf78745c6430e9d33cb706",
+    "url": "/static/media/jost-latin-ext-400-italic.2fd6c606.woff2"
+  },
+  {
+    "revision": "339a3600ce64e65f314ebce5ffe19be0",
+    "url": "/static/media/jost-latin-ext-400-normal.339a3600.woff2"
+  }
+]);
